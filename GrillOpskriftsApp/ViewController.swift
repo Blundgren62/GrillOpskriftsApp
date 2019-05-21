443bd864d1c83fc187619bab1caabb2b16663191
Blundgren <blundgren@icloud.com>
2019-05-21 21:17:25 +0200

    Initial Commit

---
+++ GrillOpskriftsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrillOpskriftsApp
+//
+//  Created by Bent Lundgren on 21/05/2019.
+//  Copyright © 2019 Bent Lundgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
