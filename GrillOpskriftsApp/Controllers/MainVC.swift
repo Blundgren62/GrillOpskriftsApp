793a8bc46a6ff73983f34c395c71412d80048a20
Blundgren <blundgren@icloud.com>
2019-06-06 06:08:55 +0200

    Så kom der tekst på mainVC

--- GrillOpskriftsApp/Controllers/MainVC.swift
+++ GrillOpskriftsApp/Controllers/MainVC.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 enum ThoughtCategory : String {
     case serious = "serious"
@@ -26,7 +26,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Variables
     private var thoughts = [Thought]()
-    
+    private var thoughtsCollectionsRef: CollectionReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +34,32 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         tableView.estimatedRowHeight = 80
         tableView.rowHeight = UITableView.automaticDimension
+        
+        thoughtsCollectionsRef = Firestore.firestore().collection(THOUGHTS_REF)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        thoughtsCollectionsRef.getDocuments { (snapshot, error) in
+            if let err = error {
+                debugPrint("Error fething doc: \(err)")
+            } else {
+                guard let snap = snapshot else { return }
+                for document in snap.documents {
+                    let data = document.data()
+                    let username = data[USERNAME] as? String ?? "Anonymous"
+                    let timestamp = data[TIMESTAMP] as? Date ?? Date()
+                    let thoughtTxt = data[THOUGHT_TXT] as? String ?? ""
+                    let numLikes = data[NUM_LIKES] as? Int ?? 0
+                    let numComments = data[NUM_COMMENTS] as? Int ?? 0
+                    let documentId = document.documentID
+                    
+                    let newThought = Thought(username: username, timestamp: timestamp, thoughtTxt: thoughtTxt, numLikes: numLikes, numComments: numComments, documentId: documentId)
+                    
+                    self.thoughts.append(newThought)
+                }
+                self.tableView.reloadData()
+            }
+        }
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
