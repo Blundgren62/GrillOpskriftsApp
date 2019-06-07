f55b4b43b07aed34632b985a0ded723ca537198a
Blundgren <blundgren@icloud.com>
2019-06-07 06:07:04 +0200

    Så kom der tekst på telefonen

--- GrillOpskriftsApp/Views/ThoughtCell.swift
+++ GrillOpskriftsApp/Views/ThoughtCell.swift
@@ -24,7 +24,9 @@ class ThoughtCell: UITableViewCell {
     }
 
     func configureCell(thought: Thought) {
-        
+        usernameLbl.text = thought.username
+        thoughtTxtLbl.text = thought.thoughtTxt
+        likesNumLbl.text = String(thought.numLikes)
     }
 
 }
